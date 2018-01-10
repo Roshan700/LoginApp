59ec828a6df0e896c578a1256aa70063e8040ab3
Roshan <roshantalreja0@gmail.com>
2018-01-10 20:44:55 +0530

    First Commit

---
+++ app/src/main/java/com/example/roshan/loginappppp/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.roshan.loginappppp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnLogin;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                    Intent go = new Intent(MainActivity.this,MainActivity.class);
+                    startActivity(go);
+
+
+
+            }
+        });
+    }
+}
